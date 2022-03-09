b22b5ff1f3f8cd4a829440160d01c818880623da
Tristan <LeoDog896@hotmail.com>
2022-03-09 17:26:02 -0500

    Make flags global

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,81 @@
-use clap::{Subcommand, Parser, Args};
-use qrcode::QrCode;
+#![deny(missing_docs)]
+
+//! A CLI wrapper around qrcode as an alternative for qrencode
+
 use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
 use qrcode::render::unicode;
+use qrcode::QrCode;
 use std::path::PathBuf;
 
 use std::fs::write;
 
 #[derive(Args, Clone)]
 struct BasicOutput {
-	/// The content of the QR code
-    content: String,
-
-	/// If the QR code should have a quiet zone (padding)
-	#[clap(short, long)]
-	quiet_zone: bool,
-
-	#[clap(short, long, parse(from_os_str))]
-	output: Option<PathBuf>
+    /// The content of the QR code
+    content: String
 }
 
 #[derive(Subcommand, Clone)]
 enum QROutputType {
-	UNICODE(BasicOutput),
-	ASCII(BasicOutput),
+    /// Print with unicode (each character = 2 pixels vertically, white & black)
+    Unicode(BasicOutput),
+    /// Print with ASCII (# and space)
+    Ascii(BasicOutput),
 }
 
 /// A simple QR code generator.
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
-struct CLI {
+struct Cli {
     /// The type of output of the QR code.
-	#[clap(subcommand)]
-	r#type: QROutputType
+    #[clap(subcommand)]
+    r#type: QROutputType,
+
+	/// If the QR code should have a quiet zone (padding)
+    #[clap(short, long, global = true)]
+    quiet_zone: bool,
+
+	/// A file output. If none is specified, it will output to stdout.
+	#[clap(short, long, parse(from_os_str))]
+    output: Option<PathBuf>,
 }
 fn main() {
-	main_err().unwrap()
+    main_err().unwrap();
 }
 
 fn main_err() -> Result<()> {
-    let args = CLI::parse();
+    let args = Cli::parse();
 
-	let options = match args.r#type.clone() {
-		QROutputType::ASCII(options) => options,
-		QROutputType::UNICODE(options) => options
-	};
+    let options = match args.r#type.clone() {
+        QROutputType::Ascii(options) | QROutputType::Unicode(options) => options,
+    };
 
-    let qr_code = match args.r#type.clone() {
-		QROutputType::ASCII(_) => 
-			QrCode::new(options.content)?.render()
-				.light_color(' ')
-				.dark_color('#')
-				.quiet_zone(options.quiet_zone)
-				.build(),
-		QROutputType::UNICODE(_) => 
-			QrCode::new(options.content)?.render::<unicode::Dense1x2>()
-				.dark_color(unicode::Dense1x2::Light)
-				.light_color(unicode::Dense1x2::Dark)
-				.quiet_zone(options.quiet_zone)
-				.build()
-		
-	};
+    let unrendered_qr_code = QrCode::new(options.content)?;
 
-	match options.output {
-		Some(file) => { write(file, qr_code)?; },
-		None => { println!("{}", qr_code); }
-	};
+    let qr_code = match args.r#type {
+        QROutputType::Ascii(_) => unrendered_qr_code
+            .render()
+            .light_color(' ')
+            .dark_color('#')
+            .quiet_zone(args.quiet_zone)
+            .build(),
+        QROutputType::Unicode(_) => unrendered_qr_code
+            .render::<unicode::Dense1x2>()
+            .dark_color(unicode::Dense1x2::Light)
+            .light_color(unicode::Dense1x2::Dark)
+            .quiet_zone(args.quiet_zone)
+            .build(),
+    };
 
-	return Ok(());
+    match args.output {
+        Some(file) => {
+            write(file, qr_code)?;
+        }
+        None => {
+            println!("{}", qr_code);
+        }
+    };
+
+    Ok(())
 }
